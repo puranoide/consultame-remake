b3ea2861c671146ca639dd238e10adbc4f2dd9c8
puranoide <acostasanchezangelgabriel@gmail.com>
2025-07-09 12:06:08 -0500

    Update db.php

--- controllers/db.php
+++ controllers/db.php
@@ -1,11 +1,14 @@
 <?php
-
+$servername = "localhost";
+$username = "u685818680_gabrieladmin02";
+$password = "7|Pvfsp~WjP";
+$dbname = "u685818680_consultamev2";
+// Create connection
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "consultame";
-
-// Create connection
+$dbname = "consultame"; */
 $conexion = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conexion->connect_error) {
