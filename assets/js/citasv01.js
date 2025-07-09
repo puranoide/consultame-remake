8b102b15bf7eea4d05385c3246922f0376c05255
puranoide <acostasanchezangelgabriel@gmail.com>
2025-07-09 12:00:44 -0500

    agendacion de cita terminada

--- assets/js/citasv01.js
+++ assets/js/citasv01.js
@@ -1,5 +1,5 @@
 var formulariocita = document.getElementById("post-form-register");
-
+var fechasDisponibles = document.getElementById("fechasDisponibles");
 formulariocita.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log("submit");
@@ -11,18 +11,62 @@ formulariocita.addEventListener("submit", function (event) {
   inputs.forEach((input) => {
     let key = input.id ? input.id : input.name;
     console.log(key, input.value);
-    objetoCita[key] = input.value;
+    if (input.type === "datetime-local") {
+      objetoCita[key] = new Date(input.value).toLocaleString();
+    } else {
+      objetoCita[key] = input.value;
+    }
   });
+
   console.log(objetoCita);
-  regitrarcita(objetoCita);
+  var fecha = document.getElementById("fechaCita").value;
+  var hora = localStorage.getItem("horaCita");
+  // Concatena la fecha y la hora en un solo string
+  var fechaHora = fecha + ' ' + hora;
+
+  // Formatea la fecha y hora en un formato compatible con MySQL
+  var fechaHoraFormateada = fechaHora.replace(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/, '$1-$2-$3 $4:$5:00');
+  console.log(fechaHoraFormateada);
+  validarFechayhora(fechaHoraFormateada, objetoCita);
+  //regitrarcita(objetoCita);
 });
 
 
+function validarFechayhora(fecha, objetoCita) {
+
+  fetch("controllers/citas.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      action: "validarFechayhora",
+      fecha: fecha,
+      ...objetoCita,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        console.log("respuesta :el medico esta disponible", data);
+        citaaguardar = { fecha, ...objetoCita }
+        regitrarcita(citaaguardar);
+      }else{
+        console.log("respuesta :el medico no esta disponible",data);  
+        
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+}
+
 function regitrarcita(objcita) {
-    console.log("enviando a backend");
+  console.log("enviando a backend");
   const datos = {
     action: "registrarCita",
-    cita: objcita,
+    ...objcita,
   };
   fetch("controllers/citas.php", {
     method: "POST",
@@ -33,14 +77,92 @@ function regitrarcita(objcita) {
   })
     .then((response) => response.json())
     .then((data) => {
-      //console.log(data);
       if (data.success) {
         console.log("respuesta :", data);
-        //alert("post exitoso");
-        //window.location.href = "gestionPosts.php";
+        crearlinkzoom(objcita,data.id);
       }
     })
     .catch((error) => {
       console.log(error);
     });
 }
+function crearlinkzoom(objcita,id) {
+  console.log("enviando a backend");
+  const datos = {
+    action: "crearlinkdezoom",
+    ...objcita,
+  };
+  fetch("controllers/citas.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datos),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        console.log("respuesta :", data);
+        alert("Cita agendada con exito");
+        guardarlinkenreunion(id,data.link);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function guardarlinkenreunion(id, link) {
+  console.log("enviando a backend");
+  const datos = {
+    action: "guardarlinkenreunion",
+    id: id,
+    link: link,
+  };
+  fetch("controllers/citas.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datos),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        console.log("respuesta al guardar link:", data);
+      }
+    })
+  
+}
+function crearHorarios() {
+  let hora = 9;
+  let minuto = 0;
+  const inputdate = document.createElement("input");
+  inputdate.type = "date";
+  inputdate.className = "form-control";
+  inputdate.id = "fechaCita";
+  fechasDisponibles.appendChild(inputdate);
+  for (let i = 0; i < 24; i++) {
+    const div = document.createElement("div");
+    div.textContent = hora + ":" + (minuto === 0 ? "00" : minuto);
+    div.className = "btn btn-outline-secondary btn-lg";
+    fechasDisponibles.appendChild(div);
+
+    minuto += 30;
+    if (minuto === 60) {
+      minuto = 0;
+      hora++;
+    }
+    div.addEventListener("click", function () {
+      localStorage.setItem("horaCita", div.textContent);
+      alert("hora seleccionada: " + div.textContent + "\nenvie los datos ");
+      console.log("click", div.textContent);
+      console.log(inputdate.value);
+    })
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  crearHorarios();
+  localStorage.removeItem("horaCita");
+});
--- controllers/citas.php
+++ controllers/citas.php
@@ -1,10 +1,102 @@
 <?php
+function guardarlink($con, $data)
+{
+    $sql = "UPDATE citasform SET linkreunion=? WHERE id=?;";
+    $stmt = mysqli_prepare($con, $sql);
+    mysqli_stmt_bind_param($stmt, "si", $data['link'], $data['id']);
+    $result = mysqli_stmt_execute($stmt);
+    if ($result) {
+        $filasAfectadas = mysqli_stmt_affected_rows($stmt);
+        if ($filasAfectadas == 1) {
+            mysqli_stmt_close($stmt);
+            return true;
+        } else {
+            mysqli_stmt_close($stmt);
+            return false;
+        }
+    } else {
+        mysqli_stmt_close($stmt);
+        return false;
+    }
+}
+function crearlinkdezoom($data)
+{
+    $clientId = 'ScQutIeWQwqFMJQ97jwNgQ';       // Reemplaza con el real
+    $clientSecret = '6Ft0c7BMeOxxC43vqgdjNQckYheMtfXR'; // Reemplaza con el real
+    $accountId = 'wm9CTj3PQoyX3lsxfb06_Q';     // Reemplaza con el real
+    $url = "https://zoom.us/oauth/token?grant_type=account_credentials&account_id=$accountId";
+    $headers = [
+        "Authorization: Basic " . base64_encode("$clientId:$clientSecret")
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
+    $response = curl_exec($ch);
+    $access_token = json_decode($response)->access_token;
+    curl_close($ch);
+
+    $hostEmail = 'acostasanchezangelgabriel@gmail.com';
+    $fechaLocal = new DateTime($data['fecha'], new DateTimeZone('America/Mexico_City'));
+    $fechaUTC = $fechaLocal->setTimezone(new DateTimeZone('UTC'))->format('Y-m-d\TH:i:s\Z');
+
+    $meetingData = [
+        'topic' => 'Consulta programada con paciente',
+        'type' => 2,
+        'start_time' => $fechaUTC,
+        'duration' => 30,
+        'timezone' => 'America/Mexico_City',
+        'settings' => [
+            'host_video' => true,
+            'participant_video' => true,
+            'join_before_host' => true
+        ]
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.zoom.us/v2/users/$hostEmail/meetings");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer $access_token",
+        "Content-Type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($meetingData));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpCode !== 201) {
+        return false;
+    } else {
+        $responseData = json_decode($response, true);
+        $link = $responseData['join_url'];
+        return $link;
+    }
+}
+function validardisponibilidad($con, $fecha, $idMedico)
+{
+    $sql = "SELECT * FROM citasform WHERE idmedico = ? AND fechayhora = ? ";
+    $stmt = mysqli_prepare($con, $sql);
+    mysqli_stmt_bind_param($stmt, "is", $idMedico, $fecha);
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    $num_rows = mysqli_stmt_num_rows($stmt);
+    if ($num_rows > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 function Registercita($con, $cita)
 {
-    $sql = "INSERT INTO citasform ( dni,email,idmedico,motivo,nombre,telefono) VALUES (?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO citasform ( dni,email,idmedico,motivo,nombre,telefono,fechayhora) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($con, $sql);
-    mysqli_stmt_bind_param($stmt, "ssissi", $cita['dni'], $cita['email'], $cita['idMedico'], $cita['motivo'], $cita['nombre'], $cita['telefono']);
+    mysqli_stmt_bind_param($stmt, "ssissis", $cita['dni'], $cita['email'], $cita['idMedico'], $cita['motivo'], $cita['nombre'], $cita['telefono'], $cita['fecha']);
     $result = mysqli_stmt_execute($stmt);
     return $result;
 }
@@ -31,7 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 exit;
             }
             try {
-                $response = Registercita($conexion, $data['cita']);
+                $response = Registercita($conexion, $data);
 
                 if ($response) {
                     echo json_encode(['success' => true, 'message' => 'registro exitoso', 'id' => $conexion->insert_id]);
@@ -42,7 +134,54 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo json_encode(['error' => $e->getMessage()]);
             }
             break;
-
+        case 'validarFechayhora':
+            if (!$conexion) {
+                echo json_encode(['error' => 'No se pudo conectar a la base de datos']);
+                exit;
+            }
+            try {
+                $response = validardisponibilidad($conexion, $data['fecha'], $data['idMedico']);
+                if ($response) {
+                    echo json_encode(['success' => false, 'message' => 'el medico no esta disponible para ese horario, por favor elija otro horario']);
+                } else {
+                    echo json_encode(['success' => true, 'message' => 'el medico esta disponible para ese horario']);
+                }
+            } catch (Exception $e) {
+                echo json_encode(['error' => $e->getMessage()]);
+            }
+            break;
+        case 'crearlinkdezoom':
+            if (!$conexion) {
+                echo json_encode(['error' => 'No se pudo conectar a la base de datos']);
+                exit;
+            }
+            try {
+                $response = crearlinkdezoom($data);
+                if ($response) {
+                    echo json_encode(['success' => true, 'message' => 'registro exitoso', 'link' => $response]);
+                } else {
+                    echo json_encode(['success' => false, 'message' => 'registro fallido']);
+                }
+            } catch (Exception $e) {
+                echo json_encode(['error' => $e->getMessage()]);
+            }
+            break;
+        case 'guardarlinkenreunion':
+            if (!$conexion) {
+                echo json_encode(['error' => 'No se pudo conectar a la base de datos']);
+                exit;
+            }
+            try {
+                $response = guardarlink($conexion, $data);
+                if ($response) {
+                    echo json_encode(['success' => true, 'message' => 'registro exitoso']);
+                } else {
+                    echo json_encode(['success' => false, 'message' => 'registro fallido']);
+                }
+            } catch (Exception $e) {
+                echo json_encode(['error' => $e->getMessage()]);
+            }
+            break;
         default:
             echo json_encode(['success' => false]);
             break;
