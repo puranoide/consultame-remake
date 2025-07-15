9c35c7727d71a67fbd112b3bf9739e72b2b82d15
puranoide <acostasanchezangelgabriel@gmail.com>
2025-07-15 00:44:48 -0500

    maquetacion recomendaciones paciente + links landingpage

---
+++ assets/js/interactiveindex.js
@@ -0,0 +1,14 @@
+var contactanosbutton = document.getElementById("botoncomofunciona");
+contactanosbutton.addEventListener("click", function () { 
+    window.scrollTo({ top: document.getElementById("comofunciona").offsetTop, behavior: "smooth" });
+});
+
+var contactanosbutton = document.getElementById("botonpostpago");
+contactanosbutton.addEventListener("click", function () { 
+    window.scrollTo({ top: document.getElementById("serviciosBeneficiosCostos").offsetTop, behavior: "smooth" });
+});
+
+var contactanosbutton = document.getElementById("btncontacto");
+contactanosbutton.addEventListener("click", function () { 
+    window.scrollTo({ top: document.getElementById("contacto").offsetTop, behavior: "smooth" });
+});
--- controllers/db.php
+++ controllers/db.php
@@ -1,14 +1,20 @@
 <?php
 $servername = "localhost";
-$username = "u685818680_gabrieladmin02";
-$password = "7|Pvfsp~WjP";
-$dbname = "u685818680_consultamev2";
+$username = "root";
+$password = "";
+$dbname = "consultame";
 // Create connection
 /*
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "consultame"; */
+$dbname = "consultame"; 
+
+
+$servername = "localhost";
+$username = "u685818680_gabrieladmin02";
+$password = "7|Pvfsp~WjP";
+$dbname = "u685818680_consultamev2";*/
 $conexion = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conexion->connect_error) {
